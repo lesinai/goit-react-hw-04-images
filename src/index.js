406318c058eec240400a4056f50374b23cfb10da
lesinai <90961075+lesinai@users.noreply.github.com>
2023-06-14 17:58:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Flip, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      autoClose={1000}
+      pauseOnFocusLoss={false}
+      transition={Flip}
+    />
   </React.StrictMode>
 );
